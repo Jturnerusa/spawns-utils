408d580ca18a4ebfe9858276f43e292c67ed863e
John Turner <jturner.usa@gmail.com>
2025-03-16 19:30:04 -0400

    init

---
+++ .dir-locals.el
@@ -0,0 +1,14 @@
+((rust-ts-mode . ((eglot-workspace-configuration
+                   .
+                   (:rust-analyzer
+                    (:check
+                     (:overrideCommand
+                      ["cargo" "clippy" "--message-format=json"]))))
+                  (fmt-executable . "rustfmt")
+                  (fmt-args . ("--edition=2021"))
+                  (eval . (eglot-ensure))
+                  (eval . (company-mode 1))
+                  (eval . (add-hook 'before-save-hook 'fmt-current-buffer nil t))
+                  (eval . (add-hook 'eglot-managed-mode-hook (lambda ()
+                                                               (eglot-inlay-hints-mode -1))
+                                    nil t)))))
