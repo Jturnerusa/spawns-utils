3da31730b2775250fb727e7e3f3ca6198ff17ff8
John Turner <jturner.usa@gmail.com>
2025-03-16 19:43:32 -0400

    rename to gentoo-utils

--- tests/vdb.rs
+++ tests/vdb.rs
@@ -1,7 +1,7 @@
 use core::{assert_eq, convert::From};
 use std::path::{Path, PathBuf};
 
-use gentoo_parsers::vdb;
+use gentoo_utils::vdb;
 
 fn contents() {
     let input = "obj /usr/share/alsa/ucm2/NXP/iMX8/Librem_5_Devkit/Librem 5 Devkit.conf 6c0d51586d94c272b160eb7ba6c61331 1739589188\ndir /a/path to something\nsym /a/path to something -> ../another path 102021\n";
