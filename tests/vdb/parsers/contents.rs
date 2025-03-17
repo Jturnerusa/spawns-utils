29378dbe3c0791170bc8771ee634fd1c870442f2
John Turner <jturner.usa@gmail.com>
2025-03-16 21:05:13 -0400

    restructure modules and tests layout

--- tests/vdb/parsers/contents.rs
+++ tests/vdb/parsers/contents.rs
@@ -1,30 +1,30 @@
 use core::{assert_eq, convert::From};
 use std::path::PathBuf;
 
-use gentoo_utils::vdb;
+use gentoo_utils::vdb::parsers::contents::{contents, Content, Dir, Obj, Sym};
 
-fn contents() {
+fn test_contents() {
     let input = "obj /usr/share/alsa/ucm2/NXP/iMX8/Librem_5_Devkit/Librem 5 Devkit.conf 6c0d51586d94c272b160eb7ba6c61331 1739589188\ndir /a/path to something\nsym /a/path to something -> ../another path 102021\n";
 
     let expected = [
-        vdb::Content::Obj(vdb::Obj {
+        Content::Obj(Obj {
             path: PathBuf::from(
                 "/usr/share/alsa/ucm2/NXP/iMX8/Librem_5_Devkit/Librem 5 Devkit.conf",
             ),
             md5: String::from("6c0d51586d94c272b160eb7ba6c61331"),
             size: 1739589188,
         }),
-        vdb::Content::Dir(vdb::Dir {
+        Content::Dir(Dir {
             path: PathBuf::from("/a/path to something"),
         }),
-        vdb::Content::Sym(vdb::Sym {
+        Content::Sym(Sym {
             src: PathBuf::from("/a/path to something"),
             dest: PathBuf::from("../another path"),
             size: 102021,
         }),
     ];
 
-    let contents = vdb::contents(input).unwrap();
+    let contents = contents(input).unwrap();
 
     for (received, expected) in contents.iter().zip(expected) {
         assert_eq!(*received, expected);
@@ -32,5 +32,5 @@ fn contents() {
 }
 
 fn main() {
-    contents();
+    test_contents();
 }
