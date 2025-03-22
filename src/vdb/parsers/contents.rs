105c17e1df4e4c9e607bdf564ad24083f40b1c6e
John Turner <jturner.usa@gmail.com>
2025-03-21 20:28:20 -0400

    move tests into rust souce files

--- src/vdb/parsers/contents.rs
+++ src/vdb/parsers/contents.rs
@@ -124,3 +124,38 @@ fn sym(input: &str) -> ParseResult<Sym> {
         .map(|(_, src, dest, size)| Sym { src, dest, size })
         .parse(input)
 }
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_contents() {
+        let input = "obj /usr/share/alsa/ucm2/NXP/iMX8/Librem_5_Devkit/Librem 5 Devkit.conf 6c0d51586d94c272b160eb7ba6c61331 1739589188\ndir /a/path to something\nsym /a/path to something -> ../another path 102021\n";
+
+        let expected = [
+            Content::Obj(Obj {
+                path: PathBuf::from(
+                    "/usr/share/alsa/ucm2/NXP/iMX8/Librem_5_Devkit/Librem 5 Devkit.conf",
+                ),
+                md5: String::from("6c0d51586d94c272b160eb7ba6c61331"),
+                size: 1739589188,
+            }),
+            Content::Dir(Dir {
+                path: PathBuf::from("/a/path to something"),
+            }),
+            Content::Sym(Sym {
+                src: PathBuf::from("/a/path to something"),
+                dest: PathBuf::from("../another path"),
+                size: 102021,
+            }),
+        ];
+
+        let (_, contents) = contents(input).unwrap();
+
+        for (received, expected) in contents.iter().zip(expected) {
+            assert_eq!(*received, expected);
+        }
+    }
+}
